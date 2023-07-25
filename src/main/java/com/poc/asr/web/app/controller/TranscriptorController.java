c1f9865f6e363f74b5b824955461aa1508cc1288
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-07-24 20:56:53 -0400

    Added ExceptionHandling over file

--- src/main/java/com/poc/asr/web/app/controller/TranscriptorController.java
+++ src/main/java/com/poc/asr/web/app/controller/TranscriptorController.java
@@ -2,12 +2,12 @@ package com.poc.asr.web.app.controller;
 
 import com.poc.asr.web.app.service.Transcriptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -21,5 +21,10 @@ public class TranscriptorController {
   @PostMapping("/")
   public List<String> transcriptFile(@RequestParam("file") MultipartFile file) {
     return transcriptor.transalte(file);
+  }
+
+  @ExceptionHandler(IOException.class)
+  public ResponseEntity<String> handleIOException(IOException ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing the file.");
   }
 }
