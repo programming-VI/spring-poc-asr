5fc8adc348ac3bb3f8387164c3c013e6e6ad3be5
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-07-24 20:50:38 -0400

    Project initialized

---
+++ src/main/java/com/poc/asr/web/app/SpringPocAsrApplication.java
@@ -0,0 +1,13 @@
+package com.poc.asr.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPocAsrApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringPocAsrApplication.class, args);
+  }
+
+}
